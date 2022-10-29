2f224709d9f35b133a5c57114612bb645bbc36c6
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-10-29 12:04:04 +0000

    context

---
+++ client/src/context/index.jsx
@@ -0,0 +1,63 @@
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+  const [walletAddress, setWalletAddress] = useState("");
+  const [battleGround, setBattleGround] = useState("bg-astral");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [step, setStep] = useState(1);
+  const [gameData, setGameData] = useState({
+    players: [],
+    pendingBattles: [],
+    activeBattle: null,
+  });
+  const [showAlert, setShowAlert] = useState({
+    status: false,
+    type: "info",
+    message: "",
+  });
+  const [battleName, setBattleName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [updateGameData, setUpdateGameData] = useState(0);
+
+  const updateCurrentWalletAddress = async () => {
+    const accounts = await window?.ethereum?.request({
+      method: "eth_requestAccounts",
+    });
+
+    if (accounts) setWalletAddress(accounts[0]);
+  };
+
+  useEffect(() => {
+    updateCurrentWalletAddress();
+
+    window?.ethereum?.on("accountsChanged", updateCurrentWalletAddress);
+  }, []);
+
+  useEffect(() => {
+    const setSmartContractAndProvider = async () => {
+      const web3Modal = new Web3Modal();
+      const connection = await web3Modal.connect();
+      const newProvider = new ethers.providers.Web3Provider(connection);
+      const signer = new newProvider.getSigner();
+
+      setProvider(newProvider);
+    };
+
+    setSmartContractAndProvider();
+  }, []);
+
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { GlobalContextProvider } from "./context";
 import { Home, CreateBattle } from "./page";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/create-battle" element={<CreateBattle />} />
-    </Routes>
+    <GlobalContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-battle" element={<CreateBattle />} />
+      </Routes>
+    </GlobalContextProvider>
   </BrowserRouter>
 );
