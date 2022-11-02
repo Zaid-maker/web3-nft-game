8aa357ba3dbfb1409154f3251b4f613022aad69b
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-11-02 09:25:01 +0000

    fix imports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,6 @@ import CustomButton from "./CustomButton";
 import CustomInput from "./CustomInput";
 import Alert from "./Alert";
 import GameLoad from "./GameLoad";
+import OnboardModal from "./OnboardModal";
 
-export { PageHOC, CustomButton, CustomInput, Alert, GameLoad };
+export { PageHOC, CustomButton, CustomInput, Alert, GameLoad, OnboardModal };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { Battleground, CreateBattle, Battle, Home, JoinBattle } from "./page";
+import { OnboardModal } from "./components";
 import { GlobalContextProvider } from "./context";
-import { Home, CreateBattle, Battleground, Battle, JoinBattle } from "./page";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <GlobalContextProvider>
+      <OnboardModal />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/create-battle" element={<CreateBattle />} />
         <Route path="/battleground" element={<Battleground />} />
         <Route path="/battle/:battleName" element={<Battle />} />
+        <Route path="/create-battle" element={<CreateBattle />} />
         <Route path="/join-battle" element={<JoinBattle />} />
       </Routes>
     </GlobalContextProvider>
