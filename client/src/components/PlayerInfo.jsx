75bbd9a052271804945e23fce86c845a0a04d5ef
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-11-02 16:52:21 +0000

    fix playerinfo component

--- client/src/components/PlayerInfo.jsx
+++ client/src/components/PlayerInfo.jsx
@@ -22,10 +22,27 @@ const PlayerInfo = ({ player, playerIcon, mt }) => {
       />
 
       <div
-        //data-for={`Health-${mt ? "1" : "2"}`}
-        //data-tip={`Health: ${player.health}`}
+        data-for={`Health-${mt ? "1" : "2"}`}
+        data-tip={`Health: ${player.health}`}
         className={styles.playerHealth}
-      ></div>
+      >
+        {[...Array(player.health).keys()].map((item, index) => (
+          <div
+            key={`player-item-${item}`}
+            className={`${styles.playerHealthBar} ${healthLevel(
+              player.health
+            )} ${marginIndexing(index)}`}
+          />
+        ))}
+      </div>
+
+      <div
+        data-for={`Mana-${mt ? "1" : "2"}`}
+        data-tip="Mana"
+        className={`${styles.flexCenter} ${styles.glassEffect} ${styles.playerMana}`}
+      >
+        {player.mana || 0}
+      </div>
 
       <ReactTooltip
         id={`Player-${mt ? "1" : "2"}`}
@@ -41,6 +58,16 @@ const PlayerInfo = ({ player, playerIcon, mt }) => {
           {player?.playerAddress?.slice(0, 10)}
         </p>
       </ReactTooltip>
+      <ReactTooltip
+        id={`Health-${mt ? "1" : "2"}`}
+        effect="solid"
+        backgroundColor="#7f46f0"
+      />
+      <ReactTooltip
+        id={`Mana-${mt ? "1" : "2"}`}
+        effect="solid"
+        backgroundColor="#7f46f0"
+      />
     </div>
   );
 };
