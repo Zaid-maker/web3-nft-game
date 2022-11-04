9d7893a89f415e26fc1959b328bc9110d8017d21
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-11-04 17:18:53 +0000

    update gameinfo component

--- client/src/components/GameInfo.jsx
+++ client/src/components/GameInfo.jsx
@@ -12,6 +12,22 @@ const GameInfo = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const navigate = useNavigate();
 
+  const handleBattleExit = async () => {
+    const battleName = gameData.activeBattle.name;
+
+    try {
+      await contract.quitBattle(battleName);
+
+      setShowAlert({
+        status: true,
+        type: "info",
+        message: `You're quitting the ${battleName}`,
+      });
+    } catch (error) {
+      setErrorMessage(error);
+    }
+  };
+
   return (
     <>
       <div className={styles.gameInfoIconBox}>
@@ -23,6 +39,44 @@ const GameInfo = () => {
             src={alertIcon}
             alt="alert icon"
             className={styles.gameInfoIconImg}
+          />
+        </div>
+      </div>
+
+      <div
+        className={`${styles.gameInfoSidebar} ${
+          toggleSidebar ? "translate-x-0" : "translate-x-full"
+        } ${styles.glassEffect} ${styles.flexBetween} backdrop-blur-3xl`}
+      >
+        <div className="flex flex-col">
+          <div className={styles.gameInfoSidebarCloseBox}>
+            <div
+              className={`${styles.flexCenter} ${styles.gameInfoSidebarClose}`}
+              onClick={() => setToggleSidebar(false)}
+            >
+              X
+            </div>
+          </div>
+
+          <h3 className={styles.gameInfoHeading}>Game Rules:</h3>
+
+          <div className="mt-3">
+            {gameRules.map((rule, index) => (
+              <p key={`game-rule-${index}`} className={styles.gameInfoText}>
+                <span className="font-bold">{index + 1}</span>. {rule}
+              </p>
+            ))}
+          </div>
+        </div>
+
+        <div className={`${styles.flexBetween} mt-10 gap-4 w-full`}>
+          <CustomButton
+            title="Change Battleground"
+            handleClick={() => navigate("/battleground")}
+          />
+          <CustomButton
+            title="Exit Battle"
+            handleClick={() => handleBattleExit()}
           />
         </div>
       </div>
