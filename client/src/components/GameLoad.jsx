a23038a19c9448994089a456fedd79fc3e51af5e
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-11-06 16:11:29 +0000

    finisg

--- client/src/components/GameLoad.jsx
+++ client/src/components/GameLoad.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import CustomButton from "./CustomButton";
+import { useGlobalContext } from "../context";
+import { player01, player02 } from "../assets";
 import styles from "../styles";
 
 const GameLoad = () => {
+  const { walletAddress } = useGlobalContext();
   const navigate = useNavigate();
 
   return (
@@ -24,6 +27,31 @@ const GameLoad = () => {
         <p className={styles.gameLoadText}>
           Protip: while you're waiting, choose your preferred battleground
         </p>
+
+        <div className={styles.gameLoadPlayersBox}>
+          <div className={`${styles.flexCenter} flex=col`}>
+            <img src={player01} className={styles.gameLoadPlayerImg} />
+            <p className={styles.gameLoadPlayerText}>
+              {walletAddress.slice(0, 30)}
+            </p>
+          </div>
+
+          <h2 className={styles.gameLoadVS}>Vs</h2>
+
+          <div className={`${styles.flexCenter} flex-col`}>
+            <img src={player02} className={styles.gameLoadPlayerImg} />
+            <p className={styles.gameLoadPlayerText}>??????????</p>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <p className={`${styles.infoText} text-center mb-5`}>OR</p>
+
+          <CustomButton
+            title="Join other battles"
+            handleClick={() => navigate("/join-battle")}
+          />
+        </div>
       </div>
     </div>
   );
