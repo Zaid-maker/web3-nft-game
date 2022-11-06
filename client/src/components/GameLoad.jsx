50f579e4b0263e9d5a10f6764f45936c92a3cd10
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-11-06 12:39:51 +0000

    game load comp

--- client/src/components/GameLoad.jsx
+++ client/src/components/GameLoad.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import CustomButton from "./CustomButton";
+import styles from "../styles";
 
 const GameLoad = () => {
-  return <div>GameLoad</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={`${styles.flexBetween} ${styles.gameLoadContainer}`}>
+      <div className={styles.gameLoadBtnBox}>
+        <CustomButton
+          title="Choose Battleground"
+          handleClick={() => navigate("/battleground")}
+          restStyles="mt-6"
+        />
+      </div>
+
+      <div className={`flex-1 ${styles.flexCenter} flex-col`}>
+        <h1 className={`${styles.headText} text-center`}>
+          Waiting for a <br /> worthy opponent...
+        </h1>
+        <p className={styles.gameLoadText}>
+          Protip: while you're waiting, choose your preferred battleground
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default GameLoad;
